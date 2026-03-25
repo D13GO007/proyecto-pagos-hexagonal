b69fb33098f7e95ef6f9823ad9b5a87aafa28dd9
ElLemon-USC <hotterlemongrab@gmail.com>
2026-03-25 17:16:36 -0500

    25/03/2026
    en fase de desarrollo, incluido mas impuestos, falta implementar mas cositas

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,9 +5,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-  origin: '*',
-});
+    origin: '*',
+  });
 
-  await app.listen(3001, '0.0.0.0');
+  await app.listen(4000, '0.0.0.0'); 
 }
 bootstrap();
