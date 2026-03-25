f4dbab89f9d6690c7bc85a02c537a7a46c15cd5b
juanarango407 <126531130+juanarango407@users.noreply.github.com>
2026-03-25 09:25:07 -0500

    Update confirmacion-pago.controller.ts

--- backend/src/pagos/confirmacion-pago.controller.ts
+++ backend/src/pagos/confirmacion-pago.controller.ts
@@ -1,1 +1,13 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { ConfirmacionPagoService } from './confirmacion-pago.service';
 
+@Controller('pagos')
+export class ConfirmacionPagoController {
+
+  constructor(private readonly service: ConfirmacionPagoService) {}
+
+  @Get('confirmar/:id')
+  confirmar(@Param('id') id: string) {
+    return this.service.confirmarPago(id);
+  }
+}
